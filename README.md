c92d61bfa3ddc337ae816492d3c9f273558a8a6c
Serene Lian <serenelian@Serenes-MacBook-Pro-14.local>
2024-04-22 12:37:25 +0800

    update due to privacy

---
+++ README.md
@@ -0,0 +1,14 @@
+# TransGNN-Lung
+This is the official code for the paper :
+Early-Stage NSCLC Patients’ Prognostic Prediction with Multi-information Using Transformer and Graph Neural Network Model.
+<br /> Authorship: MSc Jie Lian1†, MD Jiajun Deng2†, Dr Sai Kam Hui3, Dr Mohamad Koohi-Moghadam4, Dr Yunlang She2, Dr Chang Chen2*, Dr Varut Vardhanabhuti1*
+<br />1Department of Diagnostic Radiology, Li Ka Shing Faculty of Medicine, The University of Hong Kong, Hong Kong SAR, China 
+<br />2Department of Thoracic Surgery, Shanghai Pulmonary Hospital, Tongji University School of Medicine, Shanghai, China
+<br />3Department of Rehabilitation Science, Faculty of Health and Social Science, The Hong Kong Polytechnic University, China
+<br />4Faculty of Dentistry, The University of Hong Kong, Hong Kong SAR, China 
+
+
+
+## Special Contribution:
+<br /> We used the loss fucntion from https://github.com/czifan/DeepSurv.pytorch 
+<br /> We used part of code from ViT PyTorch https://github.com/lukemelas/PyTorch-Pretrained-ViT.
---
+++ loss.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+
+class Regularization(object):
+    def __init__(self, order, weight_decay):
+        ''' The initialization of Regularization class
+        :param order: (int) norm order number
+        :param weight_decay: (float) weight decay rate
+        '''
+        super(Regularization, self).__init__()
+        self.order = order
+        self.weight_decay = weight_decay
+
+    def __call__(self, model):
+        ''' Performs calculates regularization(self.order) loss for model.
+        :param model: (torch.nn.Module object)
+        :return reg_loss: (torch.Tensor) the regularization(self.order) loss
+        '''
+        reg_loss = 0
+        for name, w in model.named_parameters():
+            if 'weight' in name:
+                reg_loss = reg_loss + torch.norm(w, p=self.order)
+        reg_loss = self.weight_decay * reg_loss
+        return reg_loss
+
+
+
+class NegativeLogLikelihood(nn.Module):
+    def __init__(self, l2_reg):
+        super(NegativeLogLikelihood, self).__init__()
+        self.L2_reg = l2_reg
+        self.reg = Regularization(order=2, weight_decay=self.L2_reg)
+
+    def forward(self, risk_pred, y, e, model):
+        mask = torch.ones(y.shape[0], y.shape[0])
+        mask[(y.T - y) > 0] = 0
+        log_loss = torch.exp(risk_pred) * mask
+        log_loss = torch.sum(log_loss, dim=0) / torch.sum(mask, dim=0)
+        log_loss = torch.log(log_loss).reshape(-1, 1)
+        neg_log_loss = -torch.sum((risk_pred-log_loss) * e) / torch.sum(e)
+        l2_loss = self.reg(model)
+        return neg_log_loss + l2_loss
